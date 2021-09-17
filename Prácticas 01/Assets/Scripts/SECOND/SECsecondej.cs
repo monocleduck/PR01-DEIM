60edfbeb10f3ded8d722791d71437185fb319132
Guillermo Cabezas <guillermogeek@gmail.com>
2021-09-17 18:31:40 +0200

    Práctica 1 ejercicio 2B
    Realizado el ejercicio 2B

--- Prácticas 01/Assets/Scripts/SECOND/SECsecondej.cs
+++ Prácticas 01/Assets/Scripts/SECOND/SECsecondej.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SECsecondej : MonoBehaviour
 {
+    private int x = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        while(x > 0){
+
+            print("el numero es " + x);
+
+            x--;
+        }
+    
+        //FOR
+
+        for(int y = 20; y> 0; y--)
+        {
+            print("el numero es " + y);
+        }
+    
+    
+    
     }
 
     // Update is called once per frame
