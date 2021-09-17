58af82d2dbbea12baab5f715b5bf1bd05400683f
Guillermo Cabezas <guillermogeek@gmail.com>
2021-09-17 20:10:12 +0200

    Práctica 1 ejercicio 3B
    Realizado el ejercicio 3B

---
+++ Prácticas 01/Assets/Scripts/THIRD/THIRDsegundej.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THIRDsegundej : MonoBehaviour
+{
+    void Matematico (int Num1, int Num2,string operacion)
+    {
+        if(operacion == "/"){
+            
+            print(Num1 / Num2);
+        }
+
+        else if(operacion == "+")
+        {
+            print(Num1 + Num2);
+        }
+
+        else if (operacion == "-")
+        {
+            print(Num1 - Num2);
+        }
+        
+        else if (operacion == "*")
+        {
+            print(Num1 * Num2);
+        }
+
+        else
+        {
+            print("la opercaion entregada no es valida");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Matematico(8,5,"*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
