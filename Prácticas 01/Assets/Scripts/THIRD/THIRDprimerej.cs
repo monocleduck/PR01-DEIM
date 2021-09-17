eec45e883e79cdbbad710eefede895e88f159140
Guillermo Cabezas <guillermogeek@gmail.com>
2021-09-17 19:40:57 +0200

    Práctica 1 ejercicio 3A
    Realizado el ejercicio 3A

---
+++ Prácticas 01/Assets/Scripts/THIRD/THIRDprimerej.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THIRDprimerej : MonoBehaviour
+{
+
+    void Saludo (string hola)
+    {
+        print("hola " + hola);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Saludo("zapato");
+        Saludo("chancla");
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
